dd28f4d626a7eafc63c40824000a3ac16c3221d2
andyUCFCS <airjardon@knights.ucf.edu>
2021-03-23 17:08:36 -0400

    Con. to server and DB. login/add/search work.

--- DinnerOnDemandWeb/server.js
+++ DinnerOnDemandWeb/server.js
@@ -4,10 +4,95 @@ const cors = require('cors');
 
 const app = express();
 
-// Add server stuff
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000));
+
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const client = new MongoClient(url);
+client.connect();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+///////////////////////////////////////////////////// For Heroku deployment
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') 
+{  // Set static folder  
+    app.use(express.static('website/build'));
+    
+    app.get('*', (req, res) =>  
+    {    
+        res.sendFile(path.resolve(__dirname, 'website', 'build', 'index.html')); // changed frontend to websiteb bc thats what its called for ours
+    });
+}
+
+// **********************HARD CODED API*********************************
+var recipeList = [  'Tomato',  'Cheese',  'Apple',  'Pepper',  'Potato'];
+
+app.post('/api/addrecipe', async (req, res, next) =>
+{  
+    // incoming: userId, color  
+    // outgoing: error  
+    const { userId, recipe } = req.body;  
+    const newRecipe = {Recipe:recipe,UserId:userId};  
+    var error = '';  
+    
+    try  
+    {    
+        const db = client.db();    
+        const result = db.collection('Recipes').insertOne(newRecipe);  
+    }  
+    catch(e)  
+    {    
+        error = e.toString();  
+    }
+    recipeList.push( recipe );  
+    var ret = { error: error };  
+    res.status(200).json(ret);
+});
+
+app.post('/api/login', async (req, res, next) => 
+{  
+    // incoming: login, password  
+    // outgoing: id, firstName, lastName, error 
+    var error = '';  
+    const { login, password } = req.body;  
+    const db = client.db();  
+    const results = await db.collection('Users').find({Login:login,Password:password}).toArray();  
+    var id = -1;  
+    var fn = '';  
+    var ln = '';  
+    if( results.length > 0 )  
+    {    
+        id = results[0].UserId;    
+        fn = results[0].FirstName;    
+        ln = results[0].LastName;  
+    }  
+    var ret = { id:id, firstName:fn, lastName:ln, error:''};  
+    res.status(200).json(ret);
+});
+
+app.post('/api/searchrecipe', async (req, res, next) => 
+{  
+    // incoming: userId, search  
+    // outgoing: results[], error  
+    var error = '';  
+    const { userId, search } = req.body;  
+    var _search = search.trim();  
+    const db = client.db();  
+    const results = await db.collection('Recipes').find({"Recipe":{$regex:_search+'.*', $options:'r'}}).toArray();  
+    var _ret = [];  
+    for( var i=0; i<results.length; i++ )  
+    {    
+        _ret.push( results[i].Recipe );  
+    }  
+    var ret = {results:_ret, error:error};  
+    res.status(200).json(ret);
+});
+// **********************HARD CODED API*********************************
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -22,5 +107,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(5000); // start Node + Express server on port 5000
+//app.listen(5000); // start Node + Express server on port 5000
 
+app.listen(PORT, () => 
+{  
+    console.log('Server listening on port ' + PORT);
+});
--- DinnerOnDemandWeb/website/src/components/LoggedInName.js
+++ DinnerOnDemandWeb/website/src/components/LoggedInName.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-function LoggedInName(props) {
-    var user = {}
-    const doLogout = event => {
-        event.preventDefault();
-        alert('doLogout');
-    };
-    return (
-        <div id="loggedInDiv">
-            <span id="userName">
-                Logged In As {props.name}
-            </span><br />
-            <button type="button" id="logoutButton" class="buttons" onClick={doLogout}>
-                Log Out
-            </button>
-        </div>
+function LoggedInName()
+{    
+    var _ud = localStorage.getItem('user_data');    
+    var ud = JSON.parse(_ud);    
+    var userId = ud.id;    
+    var firstName = ud.firstName;    
+    var lastName = ud.lastName;    
+    
+    const doLogout = event =>     
+    {    
+        event.preventDefault();        
+        localStorage.removeItem("user_data")        
+        window.location.href = '/';    
+    };      
+    return(   
+    <div id="loggedInDiv">   
+    <span id="userName">Logged In As {firstName} {lastName} {userId}</span><br />   
+    <button type="button" id="logoutButton" class="buttons"      
+    onClick={doLogout}> Log Out </button>   
+    </div>  
     );
 };
 
