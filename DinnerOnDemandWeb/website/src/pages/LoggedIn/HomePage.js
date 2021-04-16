ae1abd4cab9b8da3d19afb42936d844d8c9b8b6f
Nicolas Coneo <nconeo@knights.ucf.edu>
2021-04-16 15:04:50 -0400

    Fixed NavBar import.

--- DinnerOnDemandWeb/website/src/pages/LoggedIn/HomePage.js
+++ DinnerOnDemandWeb/website/src/pages/LoggedIn/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
+import NavBar from '../../components/NavBar';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
