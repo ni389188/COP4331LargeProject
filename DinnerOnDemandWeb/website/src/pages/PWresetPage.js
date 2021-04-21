126c24f157791b57b44d702176204635deb30d77
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2021-04-21 19:03:11 -0400

    Mobile app marco (#23)
    * Created login/registration pages, added template to all to do pages. Updated App.js
    
    Basic login and registration pages were added, with navigation between them and unfinished pages. All pages added to navigation in app.js.  Template for all pages that returns to account page. Added/removed pages as needed.
    
    * Added framework for 5 main pages. Added core components.
    
    Navigation bar was implemented, with 5 pages having framework consisting of title header, an empty body todo, and footer of navigation bar. Search page was added into body. depreciated components also removed.
    
    * Fixed search page
    
    * Added skeleton and navigation between all remaining pages
    
    General layout was created for all remaining pages. Accountbutton was refactored to navigation button for use in entire app. Created recipe card and counter components
    
    * Update RegisterPage.js
    
    * Added backend and login functionality
    
    * Added functionality to register page
    
    * Set backend IP's for android emulator
    
    * Added partial authenticity
    
    * Added tokens and updated navigationbutton functionality.
    
    Tokens are added on login and removed on logout. Navigationbutton now accepts any onpress function, and pages that used this component are updated accordingly. Additionally, code to automatically login added, but localstorage is not saving tokens, so functionality is not available yet.
    
    * Update registration and profile pages
    
    added secure typing to registration and added an updated loggedinname with authentication to profile page
    
    * Merged mobiles backend into webs backend
    
    * Merged mobile user api into user api
    
    * fixed server from mobile api merge
    
    * Removed console logs from userapi
    
    * added email and password validation for register page
    
    * Update PageTitle and pages that uses it
    
    Evenly divided page title to incorporate back button more fluidly
    
    * Added layout for settings page and fixed persisting textinputs
    
    * Update app so keyboard overlaps pages
    
    * Delete mobileUserApi.js
    
    * Implemented password hashing from web server files
    
    * Update ProfilePage.js
    
    * Implemented update user api and settings page functionality
    
    * Implemented delete account api and settings page function
    
    * Added ability to change password from update api and added changepasswordpage
    
    * Added custom styling for navigation button, added backgrounds to profile/settings page, end update calls early if no data present
    
    * update forgot password page and added mock recover api
    
    * Added mailgun api
    
    * Added work in progress verification and password recovery api's
    
    Verification api successfuly verifies users, but still needs a page to display. password recovery recognizes token, but needs a page before it can update password
    
    * added redirecting to verify/reset apis to respective pages

--- DinnerOnDemandWeb/website/src/pages/PWresetPage.js
+++ DinnerOnDemandWeb/website/src/pages/PWresetPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { useParams } from "react-router-dom";
 import PageTitle from '../components/PageTitle';
 import PWreset from '../components/PWreset';
 import NavBar from '../components/NavBar'
 
 const PWresetPage = () => {
+    var match = useParams();
     return (
         <div>
             <NavBar />
