c68afd10fc62d46f894697477dfe52c4cebff1c8
carlitosr2 <60332915+carlitosr2@users.noreply.github.com>
2021-04-21 16:28:46 -0400

    updated Registerpage UI , centered,padding,spacing button color

--- DinnerOnDemandWeb/website/src/components/Register.js
+++ DinnerOnDemandWeb/website/src/components/Register.js
@@ -3,124 +3,117 @@ import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-function Register()
-{    
+function Register() {
 
     const app_name = 'cop4331din'
-    function buildPath(route)
-    {    
-        if (process.env.NODE_ENV === 'production')     
-        {        
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
-        }    
-        else    
-        {                
-            return 'http://localhost:5000/' + route;    
+    function buildPath(route) {
+        if (process.env.NODE_ENV === 'production') {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }
+        else {
+            return 'http://localhost:5000/' + route;
         }
     }
 
     var firstName;
     var lastName;
-    var email;    
+    var email;
     var password;
-    var confirmPassword;    
-    const [message,setMessage] = useState('');  
+    var confirmPassword;
+    const [message, setMessage] = useState('');
 
     // Required for email verification
     const mailgun = require("mailgun-js");
     const DOMAIN = "sandboxef514b75a9714c9282339dd8b689bcb8.mailgun.org";
-    const mg = mailgun({apiKey: "TODO", domain: DOMAIN});
+    const mg = mailgun({ apiKey: "TODO", domain: DOMAIN });
 
 
-    const doRegister = async event =>     
-    {   
+    const doRegister = async event => {
         // Stops the page from refreshing
-        event.preventDefault(); 
+        event.preventDefault();
 
         // If Passwords do not match try again
-        if (password.value !== confirmPassword.value)
-        {
+        if (password.value !== confirmPassword.value) {
             setMessage('passwords did not match');
         }
-        
+
         // If passwords matched then continue
         else {
-            
-            var obj = {FirstName: firstName.value, LastName: lastName.value, Email: email.value, Password: password.value};  
+
+            var obj = { FirstName: firstName.value, LastName: lastName.value, Email: email.value, Password: password.value };
             var js = JSON.stringify(obj);
-            
-            try        
-            {                
-                const response = await fetch(buildPath('api/register'),                
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
-            var res = JSON.parse(await response.text());        
-            
-            // TO-DO add error handling.
-            if(res.errors)            
-            {    
-                // Validator found this email already exists.
-                if (res.errors.Email.name === 'ValidatorError') {
-                    setMessage('This email already exists');  
+            try {
+                const response = await fetch(buildPath('api/register'),
+                    { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+
+                var res = JSON.parse(await response.text());
+
+                // TO-DO add error handling.
+                if (res.errors) {
+                    // Validator found this email already exists.
+                    if (res.errors.Email.name === 'ValidatorError') {
+                        setMessage('This email already exists');
+                    }
+                    // Something else happened               
+                    else {
+                        setMessage('Could not create the account');
+                    }
                 }
-                // Something else happened               
+                // The account was created.           
                 else {
-                    setMessage('Could not create the account'); 
-                }     
-            } 
-            // The account was created.           
-            else            
-            {          
-                // Setup email information to be sent to user
-                const data = {
-                    from: "Mailgun Sandbox <postmaster@sandboxef514b75a9714c9282339dd8b689bcb8.mailgun.org>",
-                    to: email.value,
-                    subject: "Email Verification",
-                    text: "Please confirm your email to activate your account! https://cop4331din.herokuapp.com"
-                };
-                setMessage('Account was successfully created! Please Verify your Account. An email has been sent to ' + email.value);   
-                // Send the email to the user
-                mg.messages().send(data, function (error, body) {
-                    console.log(body);
-                });    
-            } 
-            
-            //setMessage("the lenght is: " + JSON.stringify(res));
-        }        
-        catch(e)        
-        {            
-            alert(e.toString());            
-            return;        
-        }        
-    }       
-};
+                    // Setup email information to be sent to user
+                    const data = {
+                        from: "Mailgun Sandbox <postmaster@sandboxef514b75a9714c9282339dd8b689bcb8.mailgun.org>",
+                        to: email.value,
+                        subject: "Email Verification",
+                        text: "Please confirm your email to activate your account! https://cop4331din.herokuapp.com"
+                    };
+                    setMessage('Account was successfully created! Please Verify your Account. An email has been sent to ' + email.value);
+                    // Send the email to the user
+                    mg.messages().send(data, function (error, body) {
+                        console.log(body);
+                    });
+                }
 
-    return(     
-        <Card>
-            <Card.Title>
-                Need to Register? Sign Up Below!
-            </Card.Title>
-            <Form onSubmit={doRegister}>
-                <Form.Group controlId="firstName">
-                    <Form.Control type="text" placeholder="First Name" ref={(c) => firstName = c}/>
-                </Form.Group>
-                <Form.Group controlId="lastName" >
-                    <Form.Control type="text" placeholder="Last Name" ref={(c) => lastName = c}/>
-                </Form.Group>
-                <Form.Group controlId="loginName">
-                    <Form.Control type="text" placeholder="Email" ref={(c) => email = c}/>
-                </Form.Group>
-                <Form.Group controlId="loginPassword">
-                    <Form.Control type="password" placeholder="Password" ref={(c) => password = c}/>
-                </Form.Group>
-                <Form.Group controlId="confirmPassword">
-                    <Form.Control type="password" placeholder="Please Re-Enter Password" ref={(c) => confirmPassword = c}/>
-                </Form.Group>
-                <Button type="submit" controlId="loginButton" onClick={doRegister}>Register</Button>
-                <span id="registerResult">{message}</span>
-            </Form>
-        </Card>
+                //setMessage("the lenght is: " + JSON.stringify(res));
+            }
+            catch (e) {
+                alert(e.toString());
+                return;
+            }
+        }
+    };
+
+    return (
+        <div className="container justify-content-center ">
+            <div class="card text-white text-center bg-dark pl-5 pr-5 mt-5" >
+                 <div class="card-header ">
+                    <h4>Need to Register? Sign Up Below!</h4>
+                </div>
+                <Form onSubmit={doRegister}>
+                    <Form.Group controlId="firstName">
+                        <Form.Control type="text" placeholder="First Name" ref={(c) => firstName = c} />
+                    </Form.Group>
+                    <Form.Group controlId="lastName" >
+                        <Form.Control type="text" placeholder="Last Name" ref={(c) => lastName = c} />
+                    </Form.Group>
+                    <Form.Group controlId="loginName">
+                        <Form.Control type="text" placeholder="Email" ref={(c) => email = c} />
+                    </Form.Group>
+                    <Form.Group controlId="loginPassword">
+                        <Form.Control type="password" placeholder="Password" ref={(c) => password = c} />
+                    </Form.Group>
+                    <Form.Group controlId="confirmPassword">
+                        <Form.Control type="password" placeholder="Please Re-Enter Password" ref={(c) => confirmPassword = c} />
+                    </Form.Group>
+                    
+                    <span id="registerResult">{message}</span>
+                </Form>
+                <Button class ="btn-block btn-lg mb-5" variant="light" type="submit" controlId="loginButton" onClick={doRegister}>Register</Button>
+            </div>
+        </div>
     );
 };
-    
+
 export default Register;
