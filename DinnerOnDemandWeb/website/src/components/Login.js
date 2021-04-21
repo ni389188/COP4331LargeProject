2f4153d732ac02a4774f52f8be6ec67712006318
carlitosr2 <60332915+carlitosr2@users.noreply.github.com>
2021-04-21 15:36:21 -0400

    centered login page and updated loginPageUI

--- DinnerOnDemandWeb/website/src/components/Login.js
+++ DinnerOnDemandWeb/website/src/components/Login.js
@@ -3,82 +3,77 @@ import Card from 'react-bootstrap/Card'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-function Login()
-{    
+function Login() {
 
     const app_name = 'cop4331din'
-    function buildPath(route)
-    {    
-        if (process.env.NODE_ENV === 'production')     
-        {        
-            return 'https://' + app_name +  '.herokuapp.com/' + route;
-        }    
-        else    
-        {                
-            return 'http://localhost:5000/' + route;    
+    function buildPath(route) {
+        if (process.env.NODE_ENV === 'production') {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }
+        else {
+            return 'http://localhost:5000/' + route;
         }
     }
 
-    var loginName;    
-    var loginPassword;    
-    const [message,setMessage] = useState('');  
+    var loginName;
+    var loginPassword;
+    const [message, setMessage] = useState('');
 
-    const doLogin = async event =>     
-    {        
+    const doLogin = async event => {
         event.preventDefault();
 
-        var obj = {Email:loginName.value,Password:loginPassword.value};        
-        var js = JSON.stringify(obj);        
-        
-        try        
-        {                
-            const response = await fetch(buildPath('api/login'),                
-            {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+        var obj = { Email: loginName.value, Password: loginPassword.value };
+        var js = JSON.stringify(obj);
 
-        var res = JSON.parse(await response.text());        
-        
-        // TO-DO add error handling.
-        if(res.loggedIn === false)            
-        {                
-            setMessage('User/Password combination incorrect');            
-        }            
-        else            
-        {                
-            var user = {firstName:res.FirstName,lastName:res.LastName,id:res.ID}                
-            localStorage.setItem('user_data', JSON.stringify(user));                
-            setMessage('');                
-            window.location.href = '/pages/HomePageExtras/Home';            
-        } 
-        
+        try {
+            const response = await fetch(buildPath('api/login'),
+                { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
 
-    }        
-    catch(e)        
-    {            
-        alert(e.toString());            
-        return;        
-    }        
-};
+            var res = JSON.parse(await response.text());
+
+            // TO-DO add error handling.
+            if (res.loggedIn === false) {
+                setMessage('User/Password combination incorrect');
+            }
+            else {
+                var user = { firstName: res.FirstName, lastName: res.LastName, id: res.ID }
+                localStorage.setItem('user_data', JSON.stringify(user));
+                setMessage('');
+                window.location.href = '/pages/HomePageExtras/Home';
+            }
+
+
+        }
+        catch (e) {
+            alert(e.toString());
+            return;
+        }
+    };
 
     return (
-        <Card>
-            <Card.Title>
-                Welcome Back. Log In Below!
-            </Card.Title>
-            <Form onSubmit={doLogin}>
-                <Form.Group controlId="loginName">
-                    <Form.Control type="email" placeholder="Enter Email" ref={(c) => loginName = c} />
-                </Form.Group>
-                <Form.Group controlId="loginPassword">
-                    <Form.Control type="password" placeholder="Enter Password" ref={(c) => loginPassword = c}/>
-                </Form.Group>
-                <Button type="submit" controlId="loginButton" onClick={doLogin}>Log In</Button>
-                <span id="loginResult">{message}</span>
-                <br />
-                <a href="../pages/RegisterPage">Need to Register? Click Here</a><br/>
-                <a href="../pages/PasswordResetPage">Forgot Your Password? Click Here</a>
-            </Form>
-        </Card >
+
+        <div className="container justify-content-center bg-dark ">
+            <div class="card text-white bg-dark mt-5" >
+                <div class="card-header ">
+                    <h4>Welcome Back. Log In Below!</h4>
+                </div>
+                <Form onSubmit={doLogin}>
+                    <Form.Group controlId="loginName">
+                        <Form.Control type="email" placeholder="Enter Email" ref={(c) => loginName = c} />
+                    </Form.Group>
+                    <Form.Group controlId="loginPassword">
+                        <Form.Control type="password" placeholder="Enter Password" ref={(c) => loginPassword = c} />
+                    </Form.Group>
+                    <Button variant="light" type="submit" controlId="loginButton" onClick={doLogin}>Log In</Button>
+                    <span id="loginResult">{message}</span>
+                    <br />
+                    <a className="text-white "href="../pages/RegisterPage" >Need to Register? Click Here</a> <br />
+                    <a className="text-white" href="../pages/PasswordResetPage">Forgot Your Password? Click Here</a>
+                </Form>
+            </div>
+        </div>
+
     );
 };
-    
+
 export default Login;
