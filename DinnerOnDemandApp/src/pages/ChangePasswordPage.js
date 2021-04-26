a3316471abc704a4ec5d0ab7a4776362246d46bf
Carl Antoine <carlantoine14@gmail.com>
2021-04-26 17:48:43 -0400

    Profile pages ui

--- DinnerOnDemandApp/src/pages/ChangePasswordPage.js
+++ DinnerOnDemandApp/src/pages/ChangePasswordPage.js
@@ -1,110 +1,97 @@
-import React, {useState} from 'react';
-import { View, StyleSheet, Text, TextInput } from 'react-native';
-
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { Layout, Text } from '@ui-kitten/components';
 import PageTitle from '../components/PageTitle';
 import NavigationButton from '../components/NavigationButton';
 import jwt_decode from 'jwt-decode';
 
-const ChangePasswordPage = ({navigation}) =>
-{
+const ChangePasswordPage = ({ navigation }) => {
   const storage = require('../tokenStorage');
   const [oPassword, onChangeOPassword] = useState('');
-  const [password, onChangePassword] = useState(''); 
-  const [cPassword, onChangeCPassword] = useState(''); 
+  const [password, onChangePassword] = useState('');
+  const [cPassword, onChangeCPassword] = useState('');
   const app_name = 'cop4331din';
-  function buildPath(route)
-  {    
-    if (process.env.NODE_ENV === 'production')     
-    {        
-      return 'https://' + app_name +  '.herokuapp.com/' + route;
+  function buildPath(route) {
+    if (process.env.NODE_ENV === 'production') {
+      return 'https://' + app_name + '.herokuapp.com/' + route;
     }
-    else    
-    {                
-      return 'http://10.0.2.2:5000/' + route;  
+    else {
+      return 'http://10.0.2.2:5000/' + route;
     }
   };
-  const doSave = async event =>
-  {
+  const doSave = async event => {
     var valid = isValid()
-    if(valid != '')
-    {
+    if (valid != '') {
       alert(valid);
       return;
     }
-    if(password != cPassword)
-    {
+    if (password != cPassword) {
       alert('Passwords do not match');
       return
     }
     event.preventDefault();
     var ud = await jwt_decode(await storage.retrieveToken());
-    var obj = {OldPassword:oPassword, Password:password, _id:ud.userId};
+    var obj = { OldPassword: oPassword, Password: password, _id: ud.userId };
     var js = JSON.stringify(obj);
-    try        
-    {                
-      const response = await fetch(buildPath('api/update'), {method:'post',body:js,headers:{'Content-Type': 'application/json'}});
+    try {
+      const response = await fetch(buildPath('api/update'), { method: 'post', body: js, headers: { 'Content-Type': 'application/json' } });
       var res = JSON.parse(await response.text());
-      if(res.accessToken)            
-      {                
+      if (res.accessToken) {
         alert("Password successfully updated")
-        navigation.navigate('NavigationBar');   
-      } 
-      else
-      {
-          alert(res);
+        navigation.navigate('NavigationBar');
       }
-    }        
-    catch(e)        
-    {            
-        alert(e.toString());                 
-    }   
+      else {
+        alert(res);
+      }
+    }
+    catch (e) {
+      alert(e.toString());
+    }
   };
-  const isValid = () =>
-  {
+  const isValid = () => {
     var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]){8,}/
-    if(passwordRegex.test(password) == true)
-    {
+    if (passwordRegex.test(password) == true) {
       return '';
     }
     return "Passwords must contain at least 8 characters and one of each of the following:\n-A lowercase letter\n-An uppercase letter\n-A number\n-A special character";
   }
-  return(
-    <View style = {styles.container}>
-      <View style = {styles.header}>
-        <PageTitle 
-        text = 'Change your password' 
-        back = {1}
-        navigate = {navigation}
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <PageTitle
+          text='Change your password'
+          back={1}
+          navigate={navigation}
         />
       </View>
-      <View style = {styles.body}>
-        <View style = {styles.background}>
-            <Text style = {styles.inputTitle}> Current password</Text>
-            <TextInput 
-            style = {styles.input} 
+      <View style={styles.body}>
+        <Layout style={styles.background}>
+          <Text style={styles.inputTitle}> Current password</Text>
+          <TextInput
+            style={styles.input}
             placeholder="Please enter your current password"
-            onChangeText = {onChangeOPassword}
-            secureTextEntry = {true}
-            />
-            <Text style = {styles.inputTitle}> New password</Text>
-            <TextInput 
-            style = {styles.input} 
+            onChangeText={onChangeOPassword}
+            secureTextEntry={true}
+          />
+          <Text style={styles.inputTitle}> New password</Text>
+          <TextInput
+            style={styles.input}
             placeholder="Please enter your new password"
-            onChangeText = {onChangePassword}
-            secureTextEntry = {true}
-            />
-            <Text style = {styles.inputTitle}> Confirm new password</Text>
-            <TextInput 
-            style = {[styles.input, {marginBottom:30}]} 
+            onChangeText={onChangePassword}
+            secureTextEntry={true}
+          />
+          <Text style={styles.inputTitle}> Confirm new password</Text>
+          <TextInput
+            style={[styles.input, { marginBottom: 30 }]}
             placeholder="Please confirm your new password"
-            onChangeText = {onChangeCPassword}
-            secureTextEntry = {true}
-            />
-            <NavigationButton
-            name = "Save"
-            doFunction = {doSave}
-            />
-        </View>
+            onChangeText={onChangeCPassword}
+            secureTextEntry={true}
+          />
+          <NavigationButton
+            name="Save"
+            doFunction={doSave}
+          />
+        </Layout>
       </View>
     </View>
   );
@@ -127,21 +114,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   background: {
-    backgroundColor: 'white',
-    height : '85%',
+    height: '60%',
     borderRadius: 20,
     paddingHorizontal: 20,
   },
-  inputTitle:{
-    color: 'black',
+  inputTitle: {
     marginTop: 20,
   },
   input: {
-    color: 'black',
+    color: 'white',
     borderColor: 'grey',
     borderWidth: 2,
     borderRadius: 10,
     marginTop: 2.5,
+    backgroundColor: 'black',
+    fontSize: 18
   },
 });
 
