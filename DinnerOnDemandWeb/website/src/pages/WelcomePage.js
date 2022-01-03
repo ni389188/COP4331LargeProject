1b8523dc2f4209d0f60e2230165e300c09d9a340
ni389188 <nconeo@knights.ucf.edu>
2022-01-02 21:00:07 -0500

    Organized welcome page better and removed typos

--- DinnerOnDemandWeb/website/src/pages/WelcomePage.js
+++ DinnerOnDemandWeb/website/src/pages/WelcomePage.js
@@ -18,21 +18,64 @@ const WelcomePage = () => {
         <div className="bg-secondary">
             <NavBar />
             <PageTitle />
-            <CardDeck style={{padding: "10px"}}>
-                <Card border="light">
-                    <Card.Header>
-                        What is Dinner-On-Demand?
-                    </Card.Header>
+            <CardDeck style={{ padding: "10px" }}>
+                <h2 class="text-center" style={{ color: 'black' }}>
+                    &nbsp;&nbsp;&nbsp;Here are a few examples of recipes you may find on our platform:
+                </h2>
+            </CardDeck>
+            <br />
+            <CardDeck style={{ padding: "10px" }}>
+                <br />
+                <Card border="light" style={{ width: '30rem' }}>
+                    <Card.Header>Smackin' Burger with Fries</Card.Header>
+                    <Card.Img variant="top" src={image1} alt="" />
                     <Card.Body>
                         <Card.Text>
-                            Dinner-On-Demand is a platform for recipe lookup and design.<br />
-                            Have no idea what to cook? Dinner-On-Demand can take in a few ingredients and help you find the perfect reciepe.<br />
-                            Already have your favorite recipe? That's okay too. You can store it here to make it easy to find next time.<br />
+                            <h5>Ingredient:</h5>
+                            Brioche Bun
+                            <h5>Ingredient:</h5>
+                            16 oz of Ground Beef
+                            <h5>Ingredient:</h5>
+                            Cheddar Cheese Slice
+                            <h5>Ingredient:</h5>
+                            French Fries                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card border="light" style={{ width: '30rem' }}>
+                    <Card.Header>Gourmet Toast with Poached Egg</Card.Header>
+                    <Card.Img variant="top" src={image2} alt="" />
+                    <Card.Body>
+                        <Card.Text>
+                            <h5>Ingredient:</h5>
+                            Egg
+                            <h5>Ingredient:</h5>
+                            Avocado
+                            <h5>Ingredient:</h5>
+                            Spinach
+                            <h5>Ingredient:</h5>
+                            Multi-Grain Bread
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card border="light" style={{ width: '30rem' }}>
+                    <Card.Header>Cheddar Cheese Pizza with Chicken, Olives, and Basil</Card.Header>
+                    <Card.Img variant="top" src={image3} alt="" />
+                    <Card.Body>
+                        <Card.Text>
+                            <h5>Ingredient:</h5>
+                            Brioche Bun
+                            <h5>Ingredient:</h5>
+                            16 oz of Ground Beef
+                            <h5>Ingredient:</h5>
+                            Cheddar Cheese Slice
+                            <h5>Ingredient:</h5>
+                            French Fries
                         </Card.Text>
                     </Card.Body>
                 </Card>
             </CardDeck>
-            <br />
             <CardDeck style={{padding: "10px"}}>
                 <Card border="light" style={{ width: '30rem' }}>
                     <Card.Header>
@@ -69,60 +112,16 @@ const WelcomePage = () => {
                 </Card>
             </CardDeck>
             <br />
-            <CardDeck style={{ padding: "10px" }}>
-                <h2 class="text-center" style={{color: 'black'}}>
-                    &nbsp;&nbsp;&nbsp;Here are a few exmaples of recipes you may find on our platform:
-                </h2>
-            </CardDeck>
-
-            <CardDeck style={{ padding: "10px" }}>
-                <br />
-                <Card border="light" style={{ width: '30rem' }}>
-                    <Card.Header>Smackin' Burger with Fries</Card.Header>
-                    <Card.Img variant="top" src={image1} alt=""/>
+            <CardDeck style={{padding: "10px"}}>
+                <Card border="light">
+                    <Card.Header>
+                        What is Dinner-On-Demand?
+                    </Card.Header>
                     <Card.Body>
                         <Card.Text>
-                            <h5>Ingredient:</h5>
-                                Brioche Bun
-                            <h5>Ingredient:</h5>
-                                16 oz of Ground Beef
-                            <h5>Ingredient:</h5>
-                                Cheddar Cheese Slice
-                            <h5>Ingredient:</h5>
-                                French Fries                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                
-                <Card border="light" style={{ width: '30rem' }}>
-                    <Card.Header>Gourmet Toast with Poached Egg</Card.Header>
-                    <Card.Img variant="top" src={image2} alt="" />
-                    <Card.Body>
-                        <Card.Text>
-                            <h5>Ingredient:</h5>
-                               Egg
-                            <h5>Ingredient:</h5>
-                                Avocado
-                            <h5>Ingredient:</h5>
-                                Spinach
-                            <h5>Ingredient:</h5>
-                                Multi-Grain Bread
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                
-                <Card border="light" style={{ width: '30rem' }}>
-                    <Card.Header>Cheddar Cheese Pizza with Chicken, Olives, and Basil</Card.Header>
-                    <Card.Img variant="top" src={image3} alt="" />
-                    <Card.Body>
-                        <Card.Text>
-                            <h5>Ingredient:</h5>
-                                Brioche Bun
-                            <h5>Ingredient:</h5>
-                                16 oz of Ground Beef
-                            <h5>Ingredient:</h5>
-                                Cheddar Cheese Slice
-                            <h5>Ingredient:</h5>
-                                French Fries
+                            Dinner-On-Demand is a platform for recipe lookup and design.<br />
+                            Have no idea what to cook? Dinner-On-Demand can take in a few ingredients and help you find the perfect recipe.<br />
+                            Already have your favorite recipe? That's okay too. You can store it here to make it easy to find next time.<br />
                         </Card.Text>
                     </Card.Body>
                 </Card>
